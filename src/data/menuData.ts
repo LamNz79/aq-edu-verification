ee24565bc4284cba09c43dc6d8a6bd8521d05754
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2025-04-09 08:49:47 +0700

    50062_Prototype_Danh_muc_thang_danh_gia

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -55,7 +55,7 @@ export const menuData: I0LinkItem[] = [
     links: [
       { pageId: 1000, name: "", label: "Danh mục đơn vị", link: "", status: "Menu" },
       { pageId: 1001, name: "List of evidence types", label: " Danh mục loại minh chứng", link: "4iahruyrpf", status: "Prototype" },
-      { pageId: 1002, name: "", label: "Danh mục thang đánh giá", link: "", status: "Menu" },
+      { pageId: 1002, name: "Rating scale list", label: "Danh mục thang đánh giá", link: "4ozfpuyh8g", status: "Prototype" },
     ],
   },
 
