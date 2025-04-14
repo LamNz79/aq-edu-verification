b8349f722a894acf2ca2c1559a15027e030b00e6
Nguyễn Đức Nhật Hào <thaingoc924282@gmail.com>
2025-04-14 15:10:30 +0700

    migrate from local components to lib components

--- src/app/admin/5lrwp21o3u/page.tsx
+++ src/app/admin/5lrwp21o3u/page.tsx
@@ -1,4 +1,4 @@
-import MyPageContent from "@/components/Layouts/PageContent/MyPageContent";
+import {MyPageContent} from "aq-fe-framework/components";
 import F_5lrwp21o3u_Read from "@/modules-features/admin/5lrwp21o3u/F_5lrwp21o3u_Read";
 
 export default function Page() {
