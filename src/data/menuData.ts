a9127f5be287c90c71666dac8d9228b74a11193b
lusficer <bachhuy410@gmail.com>
2025-05-05 14:27:50 +0700

    done 90% 50872

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -4,22 +4,77 @@ export const menuData: I0LinkItem[] = [
   {
     label: "Quản lí hệ thống",
     links: [
-      { pageId: 1, name: "Account management", label: "Quản lí tài khoản", link: "core71678" },
+      {
+        pageId: 1,
+        name: "Account management",
+        label: "Quản lí tài khoản",
+        link: "core71678",
+      },
       // { label: "Danh mục quyền", link: "47643", status: "Prototype" },
-      { pageId: 2, name: "Access control level", label: "Phân quyền cấp đơn vị", link: "core38677" },
-      { pageId: 3, name: "Access control", label: "Phân quyền sử dụng", link: "core83092" },
-      { name: "Document categories", label: "Danh mục loại văn bản", link: "core18256" },
-      { pageId: 4, name: "Security regulations", label: "Quy định an toàn/ bảo mật thông tin", link: "core76318", status: "Default" },
-      { pageId: 5, name: "System updates", label: "Thông tin xây dựng, cải tiến, bảo trì hệ thống", link: "core16209", status: "Default" },
-      { pageId: 6, name: "User guide", label: "Tài liệu hướng dẫn sử dụng", link: "core40207", status: "Default" },
+      {
+        pageId: 2,
+        name: "Access control level",
+        label: "Phân quyền cấp đơn vị",
+        link: "core38677",
+      },
+      {
+        pageId: 3,
+        name: "Access control",
+        label: "Phân quyền sử dụng",
+        link: "core83092",
+      },
+      {
+        name: "Document categories",
+        label: "Danh mục loại văn bản",
+        link: "core18256",
+      },
+      {
+        pageId: 4,
+        name: "Security regulations",
+        label: "Quy định an toàn/ bảo mật thông tin",
+        link: "core76318",
+        status: "Default",
+      },
+      {
+        pageId: 5,
+        name: "System updates",
+        label: "Thông tin xây dựng, cải tiến, bảo trì hệ thống",
+        link: "core16209",
+        status: "Default",
+      },
+      {
+        pageId: 6,
+        name: "User guide",
+        label: "Tài liệu hướng dẫn sử dụng",
+        link: "core40207",
+        status: "Default",
+      },
     ],
   },
   {
     label: "Văn bản - Quy định",
     links: [
-      { pageId: 7, name: "Organizational regulations", label: "Văn bản - Quy định tổ chức", link: "core26965", status: "Default" },
-      { pageId: 8, name: "Workflow process", label: "Quy trình xử lý công việc", link: "core27311", status: "Default" },
-      { pageId: 9, name: "Form templates", label: "Tài liệu biểu mẫu", link: "core12196", status: "Default" },
+      {
+        pageId: 7,
+        name: "Organizational regulations",
+        label: "Văn bản - Quy định tổ chức",
+        link: "core26965",
+        status: "Default",
+      },
+      {
+        pageId: 8,
+        name: "Workflow process",
+        label: "Quy trình xử lý công việc",
+        link: "core27311",
+        status: "Default",
+      },
+      {
+        pageId: 9,
+        name: "Form templates",
+        label: "Tài liệu biểu mẫu",
+        link: "core12196",
+        status: "Default",
+      },
     ],
   },
   {
@@ -29,52 +84,88 @@ export const menuData: I0LinkItem[] = [
   {
     label: "Danh sách tiêu chuẩn",
     link: "yqdijiutfg",
-    status: "Prototype"
+    status: "Prototype",
   },
   {
     label: "Danh sách tiêu chí",
     link: "b99o1d0u5q",
-    status: "Prototype"
+    status: "Prototype",
   },
   {
     label: "Danh sách mốc chuẩn",
     link: "txn1uriow1",
-    status: "Prototype"
+    status: "Prototype",
   },
   {
     label: "Chu kỳ kiểm định",
     link: "5lrwp21o3u",
-    status: "Prototype"
+    status: "Prototype",
   },
   {
     label: "Đơn vị chủ trì tiêu chuẩn",
     link: "7gdid7o8tk",
-    status: "Prototype"
+    status: "Prototype",
   },
   {
     label: "Đơn vị chủ trì tiêu chí",
     link: "z7xgafshhg",
-    status: "Prototype"
+    status: "Prototype",
   },
   {
     label: "Phân công phụ trách mốc chuẩn",
     link: "i3sm0z5ns4",
-    status: "Prototype"
+    status: "Prototype",
   },
   {
     label: "Danh sách minh chứng",
     link: "nxiyjlnrik",
-    status: "Prototype"
+    status: "Prototype",
   },
   {
     label: "Danh mục hệ thống",
     links: [
-      { pageId: 999, name: "", label: "Cấu hình thông tin chủ quản", link: "k683h5xrg3", status: "Prototype" },
-      { pageId: 1000, name: "", label: "Danh mục đơn vị", link: "o4e65ewrwy", status: "Prototype" },
-      { pageId: 1001, name: "List of evidence types", label: " Danh mục loại minh chứng", link: "4iahruyrpf", status: "Prototype" },
-      { pageId: 1002, name: "Rating scale list", label: "Danh mục thang đánh giá", link: "4ozfpuyh8g", status: "Prototype" },
-      {pageId: 1003, name:"", label: "Danh mục cấu hình mail", link: "zwgpy0521g", status: "Prototype"},
-      { pageId: 1006, name: "", label: "Danh mục bộ đếm", link: "6j8jkftgnc", status: "Prototype" },
+      {
+        pageId: 999,
+        name: "",
+        label: "Cấu hình thông tin chủ quản",
+        link: "k683h5xrg3",
+        status: "Prototype",
+      },
+      {
+        pageId: 1000,
+        name: "",
+        label: "Danh mục đơn vị",
+        link: "o4e65ewrwy",
+        status: "Prototype",
+      },
+      {
+        pageId: 1001,
+        name: "List of evidence types",
+        label: " Danh mục loại minh chứng",
+        link: "4iahruyrpf",
+        status: "Prototype",
+      },
+      {
+        pageId: 1002,
+        name: "Rating scale list",
+        label: "Danh mục thang đánh giá",
+        link: "pjbnqwljej",
+        status: "Prototype",
+      },
+      {
+        pageId: 1003,
+        name: "",
+        label: "Danh mục cấu hình mail",
+        link: "zwgpy0521g",
+        status: "Prototype",
+      },
+      {
+        pageId: 1006,
+        name: "",
+        label: "Danh mục bộ đếm",
+        link: "6j8jkftgnc",
+        status: "Prototype",
+      },
     ],
   },
 ];
---
+++ src/modules-features/admin/pjbnqwljej/F_pjbnqwljej_Modal.tsx
@@ -0,0 +1,125 @@
+"use client"
+import baseAxios from '@/api/baseAxios';
+import { MRT_ColumnDef } from 'mantine-react-table';
+import { Group, Text, Textarea } from '@mantine/core';
+import MyTextInput from '@/components/Inputs/TextInput/MyTextInput';
+import { useForm } from '@mantine/form';
+import { useDisclosure } from '@mantine/hooks';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { MyButtonModal, MyDataTable } from 'aq-fe-framework/components';
+import { useEffect, useState } from 'react';
+import MyFlexColumn from '@/components/Layouts/FlexColumn/MyFlexColumn';
+interface Evidence {
+    code: string;              // Mã minh chứng
+    name: string;              // Tên minh chứng
+    fileCode: string;          // Mã file minh chứng
+    validFrom: Date;           // Ngày hiệu lực
+    validTo: Date;             // Ngày hết hạn
+    fileUrl?: string;          // Đường dẫn file (nếu có)
+    relatedUrl?: string;       // Đường dẫn liên kề (nếu có)
+    status: "Còn hạn" | "Hết hạn"; // Trạng thái
+}
+interface F_pjbnqwljej_Read {
+    id: number;
+    codeTieuChuan: string;
+    codeTieuChi: string;
+    requireCode: string;
+    requireName: string;
+    result: string;
+    reportDate: Date;
+    deadlineDate: Date;
+    personUpdate: string;
+    updateDate: Date;
+    evidences: Evidence[];
+}
+
+export default function F_rdrmqcfvux_Update({ data }: { data: F_pjbnqwljej_Read }) {
+    const queryClient = useQueryClient();
+    const disclosure = useDisclosure();
+    const formatDate = (dateStr: string | Date): string => {
+        const d = new Date(dateStr);
+        return d.toLocaleDateString("vi-VN");
+    };
+
+
+    const evidenceColumns: MRT_ColumnDef<Evidence>[] = [
+        {
+            header: "Mã minh chứng",
+            accessorKey: "code",
+        },
+        {
+            header: "Tên minh chứng",
+            accessorKey: "name",
+        },
+        {
+            header: "Mã file minh chứng",
+            accessorKey: "fileCode",
+        },
+        {
+            header: "Ngày hiệu lực",
+            accessorFn: row => formatDate(row.validFrom),
+            id: "validFrom",
+        },
+        {
+            header: "Ngày hết hạn",
+            accessorFn: row => formatDate(row.validTo),
+            id: "validTo",
+        },
+        {
+            header: "Xem file",
+            accessorFn: row => row.fileUrl,
+            id: "fileUrl",
+            // Cell: ({ cell }) => (
+            //     <a href={cell.getValue()} target="_blank" rel="noopener noreferrer">
+            //         Xem
+            //     </a>
+            // ),
+        },
+        {
+            header: "Xem liên kết",
+            accessorKey: "relatedUrl",
+            // Cell: ({ cell }) => {
+            //     const url = cell.getValue();
+            //     return url ? (
+            //         <a href={url} target="_blank" rel="noopener noreferrer">Xem</a>
+            //     ) : null;
+            // },
+        },
+        {
+            header: "Trạng thái",
+            accessorKey: "status",
+        }
+    ];
+
+
+    const form = useForm<F_pjbnqwljej_Read>({
+
+        initialValues: data,
+    });
+
+
+
+
+    return (
+        <MyButtonModal disclosure={disclosure} variant='subtle' modalSize={"80%"} label='Xem' title='Chi tiết khắc phục/ cải tiến'>
+            <MyFlexColumn gap={0} mb={5}>
+                <Text>Kết quả: {data.result}</Text>
+                <Text>Nội dung cần khắc phục/ cải tiến: Minh chứng chưa đúng nội dung báo cáo</Text>
+            </MyFlexColumn>
+
+            <Group mt={10}>
+                <Text>Tổng hợp báo cáo yêu cầu/ mốc chuẩn</Text>
+                <Textarea></Textarea>
+            </Group>
+
+            <MyDataTable
+                enableRowSelection={true}
+                enableRowNumbers={true}
+                columns={evidenceColumns}
+                data={data.evidences || []}
+
+            />
+        </ MyButtonModal>
+    )
+}
+
