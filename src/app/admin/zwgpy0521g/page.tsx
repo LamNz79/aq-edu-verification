4c0ff178faa1898f809257b42a9ddefa536990a1
pham minh lam <plam84524@gmail.com>
2025-05-05 08:15:22 +0700

    fixbug(UI): Fix import

--- src/app/admin/zwgpy0521g/page.tsx
+++ src/app/admin/zwgpy0521g/page.tsx
@@ -1,5 +1,5 @@
 import MyPageContent from "@/components/Layouts/PageContent/MyPageContent";
-import F_zwgpy0521g_Read from "@/modules-features/admin/zwgpy0521g/F_zwgpy0521g_READ";
+import F_zwgpy0521g_Read from "@/modules-features/admin/zwgpy0521g/F_zwgpy0521g_read";
 export default function Page() {
   return (
     <MyPageContent>
