f48df4765d2a596b7dc78526859639dce245fb76
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2025-05-07 11:05:44 +0700

    Update F_xwuj3dvbgs_Read.tsx

--- src/modules-features/admin/xwuj3dvbgs/F_xwuj3dvbgs_Read.tsx
+++ src/modules-features/admin/xwuj3dvbgs/F_xwuj3dvbgs_Read.tsx
@@ -54,7 +54,7 @@ export default function F_xwuj3dvbgs_Read() {
                 standardCode: "TC001",
                 criteriaCode: "C001",
                 requirementCode: "R001",
-                requirementName: "Yêu cầu 1",
+                requirementName: "Chuẩn đầu ra của CTDT được xây dựng rà soát và điều chính theo quy trình định trước trong đó có sự tham gia của các BLQ",
                 result: "Đạt",
                 note: "Hoàn thành tốt nhiệm vụ",
                 midtermReportDate: new Date(),
@@ -66,7 +66,7 @@ export default function F_xwuj3dvbgs_Read() {
                 standardCode: "TC002",
                 criteriaCode: "C002",
                 requirementCode: "R002",
-                requirementName: "Yêu cầu 2",
+                requirementName: "CDR của CTDT được phát triển rõ ràng; phù hợp với mục tiêu của CTDT; sứ mạng; tầm nhìn và chiến lược của CSDT",
                 result: "Không đạt",
                 note: "Cần cải thiện hiệu suất làm việc",
                 midtermReportDate: new Date(),
@@ -77,7 +77,7 @@ export default function F_xwuj3dvbgs_Read() {
                 standardCode: "TC003",
                 criteriaCode: "C003",
                 requirementCode: "R003",
-                requirementName: "Yêu cầu 3",
+                requirementName: "CDT của CTDT được phổ biến đến các BLQ, giảng viên và NH hiểu rõ về CDT của CTDT",
                 result: "Cần cải tiến",
                 note: "Đang trong quá trình cải tiến",
                 midtermReportDate: new Date(),
@@ -116,7 +116,7 @@ export default function F_xwuj3dvbgs_Read() {
             header: "Ngày báo cáo giữa kỳ",
             accessorKey: "midtermReportDate",
             Cell: ({ cell }) => <MyDateInput defaultValue={cell.getValue() as Date | undefined}></MyDateInput>,
-            
+
         },
         {
             header: "Ngày hoàn thành cập nhật",
@@ -154,8 +154,6 @@ export default function F_xwuj3dvbgs_Read() {
                                     data={ListOfresultQuery.data!}
                                     renderTopToolbarCustomActions={() =>
                                         <>
-                                            <>
-                                            </>
                                             <Button color="green" >Lưu</Button>
                                             <AQButtonExportData
                                                 isAllData={true}
@@ -163,8 +161,9 @@ export default function F_xwuj3dvbgs_Read() {
                                                 data={ListOfresultQuery.data!}
                                                 exportConfig={exportConfig}
                                             />
-                                            <Button color="red" leftSection={<IconTrash />}>Xóa</Button></>}
-
+                                            <Button color="red" leftSection={<IconTrash />}>Xóa</Button>
+                                        </>
+                                    }
                                 />
                             </Grid.Col>
                         </Grid>
@@ -172,6 +171,4 @@ export default function F_xwuj3dvbgs_Read() {
                 </Box>
             </Paper></>
     );
-
-
 }
