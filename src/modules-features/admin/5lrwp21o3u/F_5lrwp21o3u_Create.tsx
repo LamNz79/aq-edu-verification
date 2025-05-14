adfb58e02c6f483c484f17a39a3f23db1091b1b7
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2025-05-14 09:35:35 +0700

    50864-Dieu_chinh_Chu_ky_kiem_dinh

--- src/modules-features/admin/5lrwp21o3u/F_5lrwp21o3u_Create.tsx
+++ src/modules-features/admin/5lrwp21o3u/F_5lrwp21o3u_Create.tsx
@@ -1,45 +1,37 @@
 'use client';
 
-import {MyButtonCreate, MyDateInput, MyTextInput} from "aq-fe-framework/components";
-import { Checkbox } from "@mantine/core";
+import { MyButtonCreate, MyTextInput, MyTextArea } from "aq-fe-framework/components";
 import { useForm } from "@mantine/form";
 
-interface IChiTietChuKyKiemDinhCreate {
-    cycleId: string; // Mã chu kỳ
-    cycleName: string; // Tên chu kỳ
-    startDate: Date; // Ngày bắt đầu
-    endDate: Date; // Ngày kết thúc
-    isRepeat: boolean; // Có lặp lại hay không
+interface F_5lrwp21o3u_Create {
+    code: string; // Mã chu kỳ
+    name: string; // Tên chu kỳ
     note?: string; // Ghi chú
 }
 
 
 export default function F_5lrwp21o3u_Create() {
-    const form = useForm<IChiTietChuKyKiemDinhCreate>({
+    const form = useForm<F_5lrwp21o3u_Create>({
         initialValues: {
-            cycleId: '', // Mã chu kỳ
-            cycleName: '', // Tên chu kỳ
-            startDate: new Date(), // Ngày bắt đầu
-            endDate: new Date(), // Ngày kết thúc
-            isRepeat: false, // Có lặp lại hay không
+            code: '', // Mã chu kỳ
+            name: '', // Tên chu kỳ
             note: '', // Ghi chú
         },
         validate: {
-            cycleId: (cId:string) => cId ? null : 'Không được để trống',
-            cycleName: (cName:string) => cName ? null : 'Không được để trống',
-            startDate: (sDate:Date) => sDate ? null : 'Không được để trống',
-            endDate: (eDate:Date) => eDate ? null : 'Không được để trống',
+            code: code => code ? null : 'Không được để trống',
+            name: name => name ? null : 'Không được để trống',
         }
     });
-    
+
     return (
-        <MyButtonCreate form={form} onSubmit={() => { }} objectName='Chi tiết chu kỳ kiểm định'>
-            <MyTextInput label='Mã chu kỳ' {...form.getInputProps("cycleId")} />
-            <MyTextInput label='Tên chu kỳ' {...form.getInputProps("cycleName")} />
-            <MyDateInput label='Ngày bắt đầu' {...form.getInputProps("startDate")} />
-            <MyDateInput label='Ngày kết thúc' {...form.getInputProps("endDate")} />
-            <MyTextInput label='Ghi chú' {...form.getInputProps("note")} />
-            <Checkbox label='Lặp lại' {...form.getInputProps("isRepeat")} />
+        <MyButtonCreate
+            form={form}
+            onSubmit={() => { }}
+            objectName='chu kỳ kiểm định'
+        >
+            <MyTextInput label='Mã chu kỳ' {...form.getInputProps("code")} />
+            <MyTextInput label='Tên chu kỳ' {...form.getInputProps("name")} />
+            <MyTextArea label='Ghi chú' {...form.getInputProps("note")} />
         </MyButtonCreate>
     );
 }
