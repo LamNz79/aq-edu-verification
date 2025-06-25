8b5c6a2095f9eeebe54a8c2e9af2ea4791b3d870
HuuLuan <Plam84524@gmail.com>
2025-06-25 09:07:38 +0700

    update: dashboard title

--- src/app/admin/obf4m08gkx/page.tsx
+++ src/app/admin/obf4m08gkx/page.tsx
@@ -1,5 +1,6 @@
-import MyPageContent from "@/components/Layouts/PageContent/MyPageContent";
+"use client"
 import F_obf4m08gkx_Read from "@/modules-features/admin/obf4m08gkx/F_obf4m08gkx_Read";
+import { MyPageContent } from "aq-fe-framework/components";
 
 export default function Page() {
     return (
