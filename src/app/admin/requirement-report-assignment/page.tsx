273c6395b794aa58295ab9ed0b52c62c565b31e9
Nguyễn Đức Nhật Hào <thaingoc924282@gmail.com>
2025-07-07 14:16:26 +0700

    finish ui

---
+++ src/app/admin/requirement-report-assignment/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import RequirementReportAssignmentTable from "@/modules-features/admin/ModuleRequirementReportAssignment/RequirementReportAssignmentTable";
+import { MyPageContent } from "aq-fe-framework/components";
+
+export default function Page() {
+    return (
+        <MyPageContent>
+            <RequirementReportAssignmentTable/>
+        </MyPageContent>
+    )
+}
