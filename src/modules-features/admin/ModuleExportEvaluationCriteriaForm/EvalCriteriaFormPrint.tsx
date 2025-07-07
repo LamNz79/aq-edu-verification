eaffcd4211fcfb5f032b0846c708d0ecaba0da41
Nguyễn Đức Nhật Hào <thaingoc924282@gmail.com>
2025-07-07 11:00:14 +0700

    update typo

--- src/modules-features/admin/ModuleExportEvaluationCriteriaForm/EvalCriteriaFormPrint.tsx
+++ src/modules-features/admin/ModuleExportEvaluationCriteriaForm/EvalCriteriaFormPrint.tsx
@@ -2,7 +2,6 @@ import { MyButtonViewPDF } from "aq-fe-framework/components";
 
 export default function EvalCriteriaFormPrint() {
     return (
-        <MyButtonViewPDF src="/pdf/evaluation_form_mock.pdf">
-        </MyButtonViewPDF>
+        <MyButtonViewPDF src="/pdf/evaluation_form_mock.pdf"/>
     )
 }
