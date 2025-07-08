9e35a681ff80fed5c9aa2b45eba25ccf159f3290
Nguyễn Đức Nhật Hào <thaingoc924282@gmail.com>
2025-07-08 14:18:46 +0700

    fix menu position

--- src/data/menuData_Program.ts
+++ src/data/menuData_Program.ts
@@ -241,9 +241,9 @@ export const menuData_Program: I0LinkItem[] = [
       { label: "Quản lý Kế hoạch Cải tiến", name: "", link: "none-631417455661", status: "Menu", description: "" },
       { label: "Cập nhật cải tiến theo tiêu chí", name: "", link: "none-631417455662", status: "Menu", description: "" },
       { label: "Cập nhật Báo cáo Giữa Chu kỳ Kiểm định Chất lượng CTĐT (Biểu 15)", name: "", link: "none-631417455663", status: "Menu", description: "" },
+      { label: "Quản lý Báo cáo Giữa Chu kỳ Kiểm định Chất lượng CTĐT (Biểu 15)", name: "", link: "management-mid-cycle-quality-assurance-report", status: "Prototype", description: "" },
       { label: "Theo dõi Tiến độ Hoạt động Cải tiến", name: "", link: "none-631417455664", status: "Menu", description: "" },
       { label: "Xuất Báo cáo Thực hiện Cải tiến", name: "", link: "none-631417455665", status: "Menu", description: "" },
-      { label: "Quản lý Báo cáo Giữa Chu kỳ Kiểm định Chất lượng CTĐT (Biểu 15)", name: "", link: "management-mid-cycle-quality-assurance-report", status: "Prototype", description: "" },
     ],
   },
   {
