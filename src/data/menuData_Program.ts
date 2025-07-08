b0f874bf7b494803472b0a4f8ab001ce80196826
DinhTri <tringuyen.21092003@gmail.com>
2025-07-08 14:29:54 +0700

    add(52708): prototype Quản lý  Bảng Tổng hợp Kết quả Tự đánh giá và Đánh giá ngoài (Biểu 11)

--- src/data/menuData_Program.ts
+++ src/data/menuData_Program.ts
@@ -223,7 +223,7 @@ export const menuData_Program: I0LinkItem[] = [
       { label: "Quản lý Biên bản Ghi nhớ sau Khảo sát Sơ bộ CTĐT (Biểu 08)", name: "Manage Memorandum after Preliminary Program Survey (Form 08)", link: "program-survey-memo-08", status: "Prototype", description: "" },
       { label: "Quản lý Biên bản Hoàn thành Đợt Khảo sát Chính thức CTĐT (Biểu 09)", name: "Official Survey Completion Report (Form 09)", link: "official-survey-completion-report-09", status: "Prototype", description: "" },
       { label: "Quản lý Báo cáo Đánh giá ngoài CTĐT (Biểu 10)", name: "", link: "none-234234", status: "Menu", description: "" },
-      { label: "Quản lý  Bảng Tổng hợp Kết quả Tự đánh giá và Đánh giá ngoài (Biểu 11)", name: "", link: "none-234332424", status: "Menu", description: "" },
+      { label: "Quản lý Bảng Tổng hợp Kết quả Tự đánh giá và Đánh giá ngoài (Biểu 11)", name: "Self And External Evaluation Results", link: "self-and-external-evaluation-results-11", status: "Prototype", description: "" },
       { label: "Quản lý Nhận xét về Hồ sơ Thẩm định Kết quả Đánh giá CLCTĐT (Biểu 13)", name: "Manage Comments on Program Quality Evaluation Dossier Review (Form 13)", link: "program-eval-comments-13", status: "Prototype", description: "" },
       { label: "Quản lý Nghị quyết của Hội đồng Kiểm định Chất lượng Giáo dục (Biểu 14)", name: "Manage Resolutions of the Education Quality Accreditation Council (Form 14)", link: "accreditation-council-resolution-14", status: "Prototype", description: "" },
       { label: "Giấy Chứng nhận Kiểm định Chất lượng CTĐT", name: "", link: "none-89053775", status: "Menu", description: "" },
